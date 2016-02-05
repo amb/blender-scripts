0c66af9412dd39839a2dba751bec02a82879b96f
Tommi Hyppänen <thyppane@gmail.com>
2016-02-05 16:00:48 +0200

    Performance improvements.

--- mesh_curves.py
+++ mesh_curves.py
@@ -24,24 +24,25 @@ bl_info = {
     "description": "Set object vertex colors according to mesh curvature",
     "author": "Tommi Hyppänen (ambi)",
     "location": "3D View > Object menu > Curvature to vertex colors",
-    "version": (0, 1, 1),
+    "version": (0, 1, 2),
     "blender": (2, 74, 0)
 }
 
 """
 UI: 
-    Selection: Combined, Convexity, Concavity
-    Color coding: Gray scale, red/green
-    Separate [x]
+    Selection: Both, Convexity, Concavity
+    Color coding: grayscale, grayscale combined, red/green
+    Intensity Multiplier <X>
     Invert [x]
 """
 
 import bpy
-import bmesh
 import random
 from collections import defaultdict
 import mathutils
 import math
+import numpy as np
+import cProfile, pstats, io
 
 class CurvatureOperator(bpy.types.Operator):
     """Curvature to vertex colors"""
@@ -96,21 +97,12 @@ class CurvatureOperator(bpy.types.Operator):
         name="Invert",
         default=False)
 
-    normalized = bpy.props.BoolProperty(
-        name="Normalized",
-        default=False)
-
     @classmethod
     def poll(cls, context):
         ob = context.active_object
         return ob is not None and ob.mode == 'OBJECT'
 
-    def execute(self, context):
-        vert_to_col = defaultdict(list)
-        vert_to_edges = defaultdict(list)
-
-        mesh = context.active_object.data
-
+    def set_colors(self, mesh, fvals):
         # Use 'curvature' vertex color entry for results
         if "Curvature" not in mesh.vertex_colors:
             mesh.vertex_colors.new(name="Curvature")
@@ -118,94 +110,132 @@ class CurvatureOperator(bpy.types.Operator):
         color_layer = mesh.vertex_colors['Curvature']
         mesh.vertex_colors["Curvature"].active = True
 
-        # Map vertex colors to vertex indices
-        i = 0
+        retvalues = []
+        
+        if self.typesel == "GREY":
+            splitter = fvals>0.5
+            a_part = splitter * (fvals*2-1)*self.concavity
+            b_part = np.logical_not(splitter) * (1-fvals*2)*self.convexity
+            fvals = a_part + b_part
+            fvals *= self.intensity_multiplier
+            if self.invert:
+                fvals = 1.0 - fvals
+            
+            retvalues = np.zeros((len(fvals), 3))
+            retvalues[:,0] = fvals
+            retvalues[:,1] = fvals
+            retvalues[:,2] = fvals
+            
+        if self.typesel == "GREYC":
+            if not self.convexity:
+                fvals = np.where(fvals<0.5, 0.5, fvals)
+            if not self.concavity:
+                fvals = np.where(fvals>0.5, 0.5, fvals)
+            if not self.invert:
+                fvals = 1.0 - fvals
+            fvals = (fvals-0.5)*self.intensity_multiplier+0.5
+            retvalues = np.zeros((len(fvals), 3))
+            retvalues[:,0] = fvals
+            retvalues[:,1] = fvals
+            retvalues[:,2] = fvals
+            
+        if self.typesel == "RED":
+            splitter = fvals>0.5
+            a_part = splitter * (fvals*2-1)*self.concavity
+            b_part = np.logical_not(splitter) * (1-fvals*2)*self.convexity
+            retvalues = np.zeros((len(fvals), 3))
+            if self.invert:
+                retvalues[:,0] = 1.0 - a_part * self.intensity_multiplier
+                retvalues[:,1] = 1.0 - b_part * self.intensity_multiplier
+            else:
+                retvalues[:,0] = a_part * self.intensity_multiplier
+                retvalues[:,1] = b_part * self.intensity_multiplier            
+            retvalues[:,2] = np.zeros((len(fvals)))
+
+    
+        # write vertex colors
+        mloops = mesh.loops
+        colors = np.zeros((len(color_layer.data),3))
         for poly in mesh.polygons:
             for idx in poly.loop_indices:
-                loop = mesh.loops[idx]
-                vert_to_col[loop.vertex_index].append(i)
-                i += 1
+                colors[idx] = retvalues[mloops[idx].vertex_index]
+        colors = colors.flatten()
+        color_layer.data.foreach_set("color", colors)
+        
+        return None
+    
+    def read_verts(self, mesh):
+        mverts_co = np.zeros((len(mesh.vertices)*3), dtype=np.float)
+        mesh.vertices.foreach_get("co", mverts_co)
+        return np.reshape(mverts_co, (len(mesh.vertices), 3))      
+    
+    def read_edges(self, mesh):
+        fastedges = np.zeros((len(mesh.edges)*2), dtype=np.int) # [0.0, 0.0] * len(mesh.edges)
+        mesh.edges.foreach_get("vertices", fastedges)
+        return np.reshape(fastedges, (len(mesh.edges), 2))
+    
+    def read_norms(self, mesh):
+        mverts_no = np.zeros((len(mesh.vertices)*3), dtype=np.float)
+        mesh.vertices.foreach_get("normal", mverts_no)
+        return np.reshape(mverts_no, (len(mesh.vertices), 3))
+                
+    def execute(self, context):               
+        mesh = context.active_object.data
+        fastverts = self.read_verts(mesh)
+        fastedges = self.read_edges(mesh)
+        fastnorms = self.read_norms(mesh)
 
-        # Map edges to vertex indices
-        for edge in mesh.edges:
-            for v in edge.vertices:
-                vert_to_edges[v].append(edge)
+        # Map the other vertex on an edge to vertex index
+        # connected_verts = [[]] * len(fastverts) # FIXME: for some reason this is broken!?
+        connected_verts = []
+        for i in range(len(fastverts)):
+            connected_verts.append([])
 
+        for i in range(len(fastedges)):
+            edge = fastedges[i][0], fastedges[i][1]
+            connected_verts[edge[0]].append(edge[1])
+            connected_verts[edge[1]].append(edge[0])    
+ 
         # Main calculation
-        for vert in mesh.vertices:
-            # Get connected vertices
-            other_vert = []
-            for edge in vert_to_edges[vert.index]:
-                other_vert.append(mesh.vertices[[idx for idx in edge.vertices if idx != vert.index][0]])
-                
-            # Get dot products
-            dotps = []
-            multiplier = 100
-            for v in other_vert:
-                thisdot = (v.co-vert.co).normalized().dot(vert.normal.normalized())
-                edgelength = (v.co-vert.co).length * multiplier
-                if edgelength < 1:
-                    edgelength = 1
-                if self.normalized:
-                    dotps.append(thisdot/edgelength)
-                else:
-                    dotps.append(thisdot)
-                
-            # Sum results
-            a = sum(dotps)
-            a /= len(dotps)
-            a = 1.0 - math.acos(a)/math.pi
-            
-            # Format results
-            if self.typesel == "GREY":
-                if a>0.5:
-                    a = (a-0.5)*2.0
-                    a*=self.intensity_multiplier
-                    a*=self.concavity
-                    a = [a,a,a]
-                else:
-                    a = 1.0-a*2.0
-                    a*=self.intensity_multiplier
-                    a*=self.convexity
-                    a = [a,a,a]             
-            elif self.typesel == "GREYC":
-                a-=0.5
-                a*=self.intensity_multiplier
-                if a<0:
-                    a*=self.convexity
-                else:
-                    a*=self.concavity
-                a+=0.5
-                a = 1.0-a
-                a = [a,a,a] 
-            elif self.typesel == "RED":
-                r = 0.0
-                g = 0.0
-                if a>0.5:
-                    r = (a-0.5)*2.0
-                    r*=self.intensity_multiplier
-                else:
-                    g = 1.0-a*2.0
-                    g*=self.intensity_multiplier
+        vertcount = len(mesh.vertices)
+        angvalues = np.zeros(vertcount, dtype=np.float)
 
-                if r>1.0: r=1.0
-                if r<0.0: r=0.0
-                
-                if g>1.0: g=1.0
-                if g<0.0: g=0.0      
-                
-                if self.invert:
-                    r = 1.0 - r
-                    g = 1.0 - g          
+        """
+        for i in range(vertcount):  
+            v = np.array(connected_verts[i])
+            tvec = fastverts[v]-fastverts[i]
+            tvlen = np.reshape(np.linalg.norm(tvec, axis=1), (len(v), 1))
+            #print(tvec)
+            #tvlen = np.sqrt(np.dot(tvec,tvec.conj()))
+            thisdot = (tvec/tvlen).dot(fastnorms[i])
+            edgelength = tvlen * multiplier
+            edgelength = np.where(edgelength<1, 1, edgelength)
+            a = np.sum(thisdot/edgelength)/len(v)
+            a = 1.0 - np.arccos(a)/np.pi            
+            angvalues[i] = a
+        """
 
-                a = [r*self.concavity,g*self.convexity,0.0]     
-                                
-            if self.invert and self.typesel != "RED":
-                for i in range(3):
-                    a[i] = 1.0 - a[i]
-                
-            for col in vert_to_col[vert.index]:        
-                color_layer.data[col].color = a
+        multiplier = 100
+        minedge = 1
+        maxverts = 5
+        dotps = np.zeros(maxverts)
+        for i in range(vertcount):
+            dotps.fill(0)
+            psi = 0
+            for v in connected_verts[i]:
+                tvec = fastverts[v]-fastverts[i]
+                tvlen = np.sqrt(tvec[0]*tvec[0] + tvec[1]*tvec[1] + tvec[2]*tvec[2])
+                thisdot = (tvec/tvlen).dot(fastnorms[i])
+                edgelength = tvlen * multiplier
+                if edgelength < minedge:
+                    edgelength = minedge
+                dotps[psi] = thisdot/edgelength
+                if psi<maxverts-1:
+                    psi+=1
+            # sum results
+            angvalues[i] = 1.0 - np.arccos(np.add.reduce(dotps)/(psi+1))/np.pi
+
+        self.set_colors(mesh, angvalues)           
                 
         return {'FINISHED'}
 
@@ -223,6 +253,16 @@ def unregister():
     bpy.utils.unregister_class(CurvatureOperator)
     bpy.types.VIEW3D_MT_object.remove(add_object_button)
 
+def profile_debug():
+    pr = cProfile.Profile()
+    pr.enable()
+    bpy.ops.object.vertex_colors_curve()
+    pr.disable()
+    s = io.StringIO()
+    sortby = 'cumulative'
+    ps = pstats.Stats(pr, stream=s)
+    ps.strip_dirs().sort_stats(sortby).print_stats()
+    print(s.getvalue())
 
 if __name__ == "__main__":
     #unregister()
@@ -230,3 +270,4 @@ if __name__ == "__main__":
     #pass
     # test call
     # bpy.ops.object.vertex_colors_curve()
+    #profile_debug()
